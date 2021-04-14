a78e66537a7000a43c71a46d84f6c10131b24cb1
SJAljawhary <>
2021-04-14 11:35:09 +0000

    java

---
+++ app.js
@@ -0,0 +1,19 @@
+alert('Welcome here,Enjoy your shopping !!');
+
+
+
+var section = prompt('Please enter if you want to go to a men,women or children section??')
+
+if (section == 'men'){
+console.log('men');
+}
+else if (section == 'women'){
+  console.log('women');
+}
+else if (section == 'children'){
+  console.log('children');
+}
+
+
+
+ confirm('Did you find our site interested ?')
