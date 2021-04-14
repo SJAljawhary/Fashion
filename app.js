58005b4024239628016591316fb155d46fb891f0
SJAljawhary <>
2021-04-14 12:38:57 +0000

    editing

--- app.js
+++ app.js
@@ -5,18 +5,27 @@ alert('Welcome here,Enjoy your shopping !!');
 var section = prompt('Please enter if you want to go to a men,women or children section??')
 
 if (section == 'men'){
-console.log('men');
+console.log('section '+'men');
 }
 else if (section == 'women'){
-  console.log('women');
+  console.log('section'+'women');
 }
 else if (section == 'children'){
-  console.log('children');
+  console.log('section'+'children');
 }
 
-var myWindow = window.open("", "", "width=200, height=100");
-myWindow.document.write("<p>Our new brands!</p>");
-myWindow.blur();
+
+var age = prompt('please enter if your age is less than 30 or more ?')
+
+if(age >= '45' && section == 'men'){
+  alert('Thank you alot !');
+  console.log('morethan');
+}
+
+else if(age >= '45'&& section =='women'){
+  alert('Thank you very much !');
+  console.log('lessthan');
+}
 
 
  confirm('Did you find our site interested ?')
