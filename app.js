887de6ccc0589cafa8fb6432cb984472e70be2d7
SJAljawhary <>
2021-04-15 13:26:03 +0000

    adding

--- app.js
+++ app.js
@@ -30,3 +30,63 @@ else if(age >= '45'&& section =='women'){
 
  confirm('Did you find our site interested ?')
 
+
+
+
+ var section= prompt('Please enter if you want to go to a men,women or children section??');
+
+while(section !=='men'&& section !=='women' && section !=='children'){
+
+section = prompt('Please enter if you want to go to a men,women or children section??');
+
+}
+
+console.log(section);
+
+
+
+
+var dilevery = prompt('how many times you want us to dilever weekly?') 
+
+for (let i =0; i<=dilevery; i++){
+
+  if(dilevery <='3'){
+
+ let image=' <img  src="https://external-preview.redd.it/a-CWbn39-IaF6plSXyz9JJ6AauDs6KSOf93w9MZAGLc.png?auto=webp&s=c4e27ead7a2b027c8f6595e90854490056e55064" alt="men"width=300px >'
+  
+   document.write(image)
+  }
+
+  else if(opens >= '3'){
+   let image='<img src="https://www.moneycrashers.com/wp-content/uploads/2019/03/designer-kids-clothes-1068x713.jpg" alt="children" width=300px >'
+
+   document.write(image)
+
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
